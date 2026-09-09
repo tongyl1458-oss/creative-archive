d888519fd4b91d65687973e6051c8241fd301484
tongyl1458-oss <tongyl1458@gmail.com>
2026-09-09 10:11:34 +0800

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/creative-archive/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
